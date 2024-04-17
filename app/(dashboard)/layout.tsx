9ca737561d7f06df2ffe6563af0850812aa5fd99
TaronVardanyan <tarokavardanyan@gmail.com>
2024-04-17 22:16:42 +0400

    add sidebar and mobile sidebar, also add changes in prettier config

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import Navbar from '@/components/navbar'
+import Sidebar from '@/components/sidebar'
+
+interface DashboardLayoutProps {
+  children: ReactNode
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <div className="relative h-full">
+      <div className="z-[80] hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
+        <Sidebar />
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default DashboardLayout
