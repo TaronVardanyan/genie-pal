8f54371c0eaa1eb88a4f529abcddba9e868f302b
TaronVardanyan <tarokavardanyan@gmail.com>
2024-04-18 11:30:42 +0400

    add dashboard structure

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,6 +13,7 @@ import {
   Code,
   Settings,
 } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 
 const montserrat = Montserrat({
   weight: '600',
@@ -64,6 +65,8 @@ const routes = [
 ]
 
 const Sidebar = () => {
+  const pathname = usePathname()
+
   return (
     <div className="flex h-full flex-col space-y-4 bg-[#1F2340] py-4 text-white">
       <div className="flex-1 px-3 py-2">
@@ -80,7 +83,12 @@ const Sidebar = () => {
             <Link
               key={route.href}
               href={route.href}
-              className="group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white"
+              className={cn(
+                'group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white',
+                pathname === route.href
+                  ? 'bg-white/10 text-white'
+                  : 'text-zinc-400',
+              )}
             >
               <div className="flex flex-1 items-center">
                 <route.icon className={cn('mr-3 h-5 w-5', route.color)} />
